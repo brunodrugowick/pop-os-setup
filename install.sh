82a0849b12362c9bb36c472c6f1a8d299a3c3cf5
Bruno Drugowick <drugo@pop-os.localdomain>
2021-01-24 01:48:44 -0300

    First version

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# A list of packages separated by a single space
+packages="vim git lm-sensors lshw-gtk jq httpie"
+
+# Install previous list of packages
+sudo apt install -y ${packages}
+
+# There's a bug with media keys. This solves the problem. Based on: https://askubuntu.com/questions/906723/fn-media-keys-slow-delayed-on-ubuntu-gnome-17-04/1072160#1072160
+# Comments line with '{ Scroll_Lock }'
+sudo sed -i '/{ Scroll_Lock }/s/^#*/#/g' /usr/share/X11/xkb/symbols/br
+setxkbmap
+
+flatpak_packages="com.jetbrains.IntelliJ-IDEA-Ultimate com.visualstudio.code com.valvesoftware.Steam com.spotify.Client"
+
+flatpak install -y ${flatpak_packages}
+
