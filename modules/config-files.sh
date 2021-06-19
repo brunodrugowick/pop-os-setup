e0c596a4c7578a7c118848202e20f3454b9bdef0
Bruno Drugowick <bruno.drugowick@gmail.com>
2021-06-19 10:12:29 -0300

    fix direnv config location

--- modules/config-files.sh
+++ modules/config-files.sh
@@ -6,8 +6,3 @@ cp ./templates/tmux.config $HOME/.tmux.config
 # Reset .bashrc
 cp /etc/skel/.bashrc $HOME/.bashrc
 
-# Append direnv things to ~/.bashrc
-if ! grep -q "direnv hook bash" $HOME/.bashrc; then
-    printf "\n# direnv stuff\neval \"\$(direnv hook bash)\"\n" >> $BASHRC
-fi
-
--- modules/so-packages.sh
+++ modules/so-packages.sh
@@ -6,12 +6,17 @@ sudo apt upgrade -y
 # A list of packages separated by a single space
 packages="vim git lm-sensors lshw-gtk jq httpie gnome-tweaks menulibre steam-installer tmux direnv discord"
 
+# direnv requires integration with bash
+if ! grep -q "direnv hook bash" $HOME/.bashrc; then
+    printf "\n# direnv stuff\neval \"\$(direnv hook bash)\"\n" >> $BASHRC
+fi
+
 # Install previous list of packages
 sudo apt install -y ${packages}
 
 # A list of flatpak packages separated by spaces. Use the package ID for installation.
 # You may use 'flatpak search <package-name>' if you'd like to search a package name.
-flatpak_packages="com.jetbrains.IntelliJ-IDEA-Ultimate com.visualstudio.code com.spotify.Client org.telegram"
+flatpak_packages="com.jetbrains.IntelliJ-IDEA-Ultimate com.jetbrains.GoLand com.visualstudio.code com.spotify.Client org.telegram"
 
 # Install previous list of flatpak packages.
 flatpak install -y ${flatpak_packages}
