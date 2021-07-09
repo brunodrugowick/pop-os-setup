8ed412a83a115152f7b53e3cfb8b89b66eb08a89
Bruno Drugowick <bruno.drugowick@gmail.com>
2021-07-08 23:30:43 -0300

    Removes ability to run modules independently

--- install.sh
+++ install.sh
@@ -3,68 +3,33 @@
 BASHRC=$HOME/.bashrc
 SETUP_HOME=$(pwd)
 
-if [[ "$#" -eq 0 ]]; then
-	echo -e "\nUse with 'all' to install everything or add one or more of the following parameters:"
-	echo ""
-	echo -e "\tso-packages:\tinstall APT and Flatpak packages"
-	echo -e "\tcustom:\t\tfix bugs and other custom stuff"
-	echo -e "\tconfig-files:\tset configuration files for programs such as tmux and vim"
-	echo -e "\tjava:\t\tinstall Java things"
-	echo -e "\tnode:\t\tinstall Node things"
-	echo -e "\tgnome-settings:\tset Gnome settings that I like"
-	echo -e "\tgolang:\t\tinstall Golang things"
-	echo -e "\tbash-functions:\tinstall custom Bash functions that make my life easier"
-	echo -e "\tepic-store:\tinstall legendary, an alternative launcher for Epic Games"
-	echo -e "\tdocker:\t\tinstall docker"
-	echo ""
-
-	exit 1
-fi
+echo -e "\nThis script is going to install the following:"
+echo ""
+echo -e "\tso-packages:\tinstall APT and Flatpak packages"
+echo -e "\tcustom:\t\tfix bugs and other custom stuff"
+echo -e "\tconfig-files:\tset configuration files for programs such as tmux and vim"
+echo -e "\tjava:\t\tinstall Java things"
+echo -e "\tnode:\t\tinstall Node things"
+echo -e "\tgnome-settings:\tset Gnome settings that I like"
+echo -e "\tgolang:\t\tinstall Golang things"
+echo -e "\tbash-functions:\tinstall custom Bash functions that make my life easier"
+echo -e "\tepic-store:\tinstall legendary, an alternative launcher for Epic Games"
+echo -e "\tdocker:\t\tinstall docker"
+echo ""
+echo -e "\nThis script can safelly run multiple times, but be aware it will rewrite things like your ~/.bashrc file, for example"
 
 set -ex
 
-while [[ "$#" -gt 0 ]]; do
-	case $1 in
-		# TODO better 'all' handling - if used after other params it reinstall some packages
-		all)
-			set -- so-packages custom config-files java node gnome-settings golang bash-functions epic-store
-			;;
-		so-packages) 
-			source ./modules/so-packages.sh 
-			;;
-		custom) 
-			source ./modules/custom.sh 
-			;;
-		config-files) 
-			source ./modules/config-files.sh 
-			;;
-		java) 
-			source ./modules/java.sh 
-			;;
-		node) 
-			source ./modules/node.sh 
-			;;
-		gnome-settings) 
-			source ./modules/gnome-settings.sh 
-			;;
-		golang) 
-			source ./modules/golang.sh 
-			;;
-		bash-functions) 
-			source ./modules/bash-functions.sh 
-			;;
-		epic-store)
-			source ./modules/epic-store.sh
-			;;
-		docker)
-			source ./modules/docker.sh
-			;;
-		*) 
-			echo "Unknown parameter: $1. Ignoring and continuing..."
-			;;
-	esac
-	shift # on to the next parameter
-done
+source ./modules/so-packages.sh 
+source ./modules/custom.sh 
+source ./modules/config-files.sh 
+source ./modules/java.sh 
+source ./modules/node.sh 
+source ./modules/gnome-settings.sh 
+source ./modules/golang.sh 
+source ./modules/bash-functions.sh 
+source ./modules/epic-store.sh
+source ./modules/docker.sh
 
 # Source .bashrc at the end
 source $BASHRC
