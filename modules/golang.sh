939169b6517a265b7cdd4b368b6fb78db7e88d4b
Bruno Drugowick <bruno.drugowick@gmail.com>
2021-07-08 22:05:01 -0300

    Download to same place to avoind multiple downloads on multiple runs

--- modules/golang.sh
+++ modules/golang.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 GO_VERSION=1.16.5
-TEMP_FOLDER=$(mktemp -d)
 
 echo "Installing go v${GO_VERSION}"
 sudo rm -rf /usr/local/go
-cd $TEMP_FOLDER
 wget --progress=bar:noscroll -N https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
 # 'tar -C <DIR>' changes to DIR before (since -C is order sensitive) the other operations
 sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
@@ -20,5 +18,3 @@ fi
 # Remove temp folder
 rm -rf $TEMP_FOLDER
 
-# Go back to setup directory
-cd $SETUP_HOME
