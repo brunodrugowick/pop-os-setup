58f0526a8f46245c44646a77ff57626c51e59e79
Bruno Drugowick <bruno.drugowick@gmail.com>
2021-07-10 11:19:47 -0300

    add ksnip to the list of installed packages

--- modules/so-packages.sh
+++ modules/so-packages.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # A list of packages separated by a single space
-packages="vim git lm-sensors lshw-gtk jq httpie gnome-tweaks menulibre steam-installer tmux direnv discord virtualbox"
+packages="vim git lm-sensors lshw-gtk jq httpie gnome-tweaks menulibre steam-installer tmux direnv discord virtualbox ksnip"
 
 # direnv requires integration with bash
 if ! grep -q "direnv hook bash" $BASHRC; then
