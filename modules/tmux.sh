b5483493d6e3464178bd78fe84d4da9d29307f49
Bruno Drugowick Muniz <bruno.drugowick@gmail.com>
2021-12-17 11:31:20 -0300

    Update tmux.sh

--- modules/tmux.sh
+++ modules/tmux.sh
@@ -6,7 +6,7 @@ rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Copy config files
-cp $SETUP_HOME/templates/tmux.config $HOME/.tmux.config
+cp $SETUP_HOME/templates/tmux.config $HOME/.tmux.conf
 
 # Start TMUX by default
 bash_content="
