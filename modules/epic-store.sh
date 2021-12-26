6c398f509810730cf8a86ca05fe4d01d29fe84bc
Bruno Drugowick Muniz <bruno.drugowick@gmail.com>
2021-12-26 13:56:12 -0300

    Fix Epic Store script

--- modules/epic-store.sh
+++ modules/epic-store.sh
@@ -15,13 +15,13 @@ else
 fi
 pip install .
 # It appears that the installation doesn't create the config file, so, creating an empty one.
-echo "" >> /home/drugo/.config/legendary/config.ini
+echo "" >> $HOME/.config/legendary/config.ini
 
-# restore original directory
-cd $SETUP_HOME
-
+BASHRC=$HOME/.bashrc
 # This is a best effort approach to not duplicate things =/
 if ! grep -q "export -f get-epic-game-name" $BASHRC; then
     cat ./templates/epic-store-bash-function.sh.template >> $BASHRC
 fi
 
+# restore original directory
+cd -
