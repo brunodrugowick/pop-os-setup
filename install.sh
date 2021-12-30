f3ad3e2709d3c9f9c1fbdb5232ce782106732b1b
Bruno Drugowick <bruno.drugowick@gmail.com>
2021-12-30 00:48:46 -0300

    Fix identation from TAB to SPACES

--- install.sh
+++ install.sh
@@ -150,7 +150,7 @@ TMUX_BASH_CONTENT=$(cat <<- 'EOF'
 
 # TMUX as default terminal
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-	tmux attach -t default || tmux new -s default
+  tmux attach -t default || tmux new -s default
 fi
 EOF
 )
