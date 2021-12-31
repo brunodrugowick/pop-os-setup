316400b818cbfc07697e19f8b0a8747ef3bfa1d7
Bruno Drugowick <bruno.drugowick@gmail.com>
2021-12-31 00:44:48 -0300

    gh-compare only copies URL by default

--- install.sh
+++ install.sh
@@ -107,37 +107,41 @@ BASHRC_CONTENT=$(cat <<- 'EOF'
 
 # Re-set PS1 to my liking
 function parse_git_branch () {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 if [ "$color_prompt" = yes ]; then
-    PS1='\[\033[01;34m\]\w\[\033[00m\] $(parse_git_branch) \n >> '
+  PS1='\[\033[01;34m\]\w\[\033[00m\] $(parse_git_branch) \n >> '
 else
-    PS1='\w $(parse_git_branch) \n >> '
+  PS1='\w $(parse_git_branch) \n >> '
 fi
 unset color_prompt force_color_prompt
 
 # github-compare
 function gh-compare () {
-    if [ ! -d .git ]; then
-        echo "Not a git repository"
-        return
-    fi;
+  if [ ! -d .git ]; then
+    echo "Not a git repository"
+    return
+  fi;
 
-    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-    echo "Current branch is ${CURRENT_BRANCH}"
-    MAIN_BRANCH=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
-    echo "Main branch is ${MAIN_BRANCH}"
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  echo "Current branch is ${CURRENT_BRANCH}"
+  MAIN_BRANCH=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
+  echo "Main branch is ${MAIN_BRANCH}"
 
-    if [[ "$CURRENT_BRANCH" == "$MAIN_BRANCH" ]]; then
-	echo "Already in ${MAIN_BRANCH}, go to another branch to compare"
-	return
-    fi;
+  if [[ "$CURRENT_BRANCH" == "$MAIN_BRANCH" ]]; then
+  	echo "Already in ${MAIN_BRANCH}, go to another branch to compare"
+	  return
+  fi;
 
-    GIT_URL=$(git config --local remote.origin.url | cut -d "@" -f 2 | cut -d "." -f "1-2" | sed "s/:/\//")
-    URL="https://${GIT_URL}/compare/${MAIN_BRANCH}...${CURRENT_BRANCH}"
+  GIT_URL=$(git config --local remote.origin.url | cut -d "@" -f 2 | cut -d "." -f "1-2" | sed "s/:/\//")
+  URL="https://${GIT_URL}/compare/${MAIN_BRANCH}...${CURRENT_BRANCH}"
+  if [[ "$1" != "" ]]; then
     echo "Opening ${URL}"
-
     firefox -new-window ${URL}
+  else
+    echo "URL copied to clipboard"
+    echo $URL | xclip -sel clip
+  fi;
 }
 export -f gh-compare
 
