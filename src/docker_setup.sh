c7fb9ae4f9f9d226c50195c1f883d9d89c3c9661
Bruno Drugowick <bruno.drugowick@gmail.com>
2022-01-30 15:32:13 -0300

    Move bitwarden, docker and programming to separate files

---
+++ src/docker_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install and sets up Docker
+function docker_setup () {
+  # Install via convenience script
+  TDIR=$(mktemp -d)
+  TZIPFILE=$TDIR/get-docker.sh
+  curl -fsSL https://get.docker.com -o $TZIPFILE
+  chmod +x $TZIPFILE
+  sudo $TZIPFILE
+
+  # Allowing $USER to run docker
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  newgrp docker
+}
+# TODO This script creates a nested session
+#docker_setup
---
+++ src/setup_programming.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -z "$GO_VERSION" ]; then
+  echo "Standalone run. Setting up vars"
+  # Make sure to update config.properties if you change this section of the script
+
+  BASHRC=$HOME/.bashrc
+  GO_VERSION=1.16.5
+fi
+
+# SDKMAN (Java)
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java
+
+# NVM (Node)
+NVM_VERSION="v0.39.1"
+if [ ! -d "$HOME/.nvm" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  source "$HOME/.bashrc"
+  nvm install node
+fi;
+
+# Golang
+echo "Installing go v${GO_VERSION}"
+sudo rm -rf /usr/local/go
+wget --progress=bar:noscroll -N https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
+# 'tar -C <DIR>' changes to DIR before (since -C is order sensitive) the other operations
+sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+mkdir -p $HOME/go
+if ! grep -q "/usr/local/go/bin" $BASHRC; then
+    printf "\n# Golang stuff\nexport PATH=\$PATH:/usr/local/go/bin\n" >> $BASHRC
+    printf "export GOPATH=$HOME/go\n" >> $BASHRC
+fi;
+
+# GIT
+GIT_USER="Bruno Drugowick"
+GIT_EMAIL="bruno.drugowick@gmail.com"
+GIT_CONFIG=$(git config --list | grep 'user.name\|user.email')
+if [[ -z "$GIT_CONFIG" ]]; then
+  #read -p "Username for git: " GIT_USER
+  #read -p "Email for git: " GIT_EMAIL
+  git config --global user.name "$GIT_USER"
+  git config --global user.email "$GIT_EMAIL"
+fi;
